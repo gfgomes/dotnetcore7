6577e765ba0ce14896979e4555d3470b4cdb398a
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-04-21 20:59:39 -0300

    firt commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+    }
+
+    public string Index()
+    {
+        return "Hello World!";
+    }
+
+   
+}
