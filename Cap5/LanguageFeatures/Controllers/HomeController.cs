fc6a1fcb5dae0d1fceffa3be2ded1807d884bbe4
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-06-09 23:13:49 -0300

    Configura projeto MVC

---
+++ Cap5/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View(new string[] { "C#", "Language", "Features" });
+        }
+    }
+}
