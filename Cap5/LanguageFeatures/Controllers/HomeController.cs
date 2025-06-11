235fa36848ed3da4a05818c7c32e8f70ca62354e
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-06-11 09:51:11 -0300

    Desativando avisos de análise de estado nulo

--- Cap5/LanguageFeatures/Controllers/HomeController.cs
+++ Cap5/LanguageFeatures/Controllers/HomeController.cs
@@ -7,7 +7,8 @@ namespace LanguageFeatures.Controllers
         public ViewResult Index()
         {
             Product?[] products = Product.GetProducts();
-            return View(new string[] { products[0]!.Name });
+            #pragma warning disable CS8602
+            return View(new string[] { products[0].Name });
         }
     }
 }
