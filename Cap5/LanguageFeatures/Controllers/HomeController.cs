ec0851d846dd0d3a7445fc1c4bf8f23d3f9aa3cb
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-06-12 09:57:23 -0300

    Testando um tipo com is

--- Cap5/LanguageFeatures/Controllers/HomeController.cs
+++ Cap5/LanguageFeatures/Controllers/HomeController.cs
@@ -6,16 +6,18 @@ namespace LanguageFeatures.Controllers
 
         public ViewResult Index()
         {
-            Dictionary<string, Product> products = new()
+
+            object[] data = new object[] { 275M, 29.95M,"apple", "orange", 100, 10 };
+            decimal total = 0;
+            for (int i = 0; i < data.Length; i++)
             {
-                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
-                ["Lifejacket"] = new Product
+                if (data[i] is decimal d)
                 {
-                    Name = "Lifejacket",
-                    Price = 48.95M
+                    total += d;
                 }
-            };
-            return View("Index", products.Keys);
+            }
+
+            return View("Index", new string[] { $"Total: {total:C2}" });
         }
     }
 }
