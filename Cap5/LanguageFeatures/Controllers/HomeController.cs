5fbb389e3b144fabdf988a3d67eb6467aee4c532
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-06-12 09:49:17 -0300

    Usando uma nova expressão com new

--- Cap5/LanguageFeatures/Controllers/HomeController.cs
+++ Cap5/LanguageFeatures/Controllers/HomeController.cs
@@ -6,16 +6,15 @@ namespace LanguageFeatures.Controllers
 
         public ViewResult Index()
         {
-            Dictionary<string, Product> products
-                    = new Dictionary<string, Product>
-                    {
-                        ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
-                        ["Lifejacket"] = new Product
-                        {
-                            Name = "Lifejacket",
-                            Price = 48.95M
-                        }
-                    };
+            Dictionary<string, Product> products = new()
+            {
+                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Lifejacket"] = new Product
+                {
+                    Name = "Lifejacket",
+                    Price = 48.95M
+                }
+            };
             return View("Index", products.Keys);
         }
     }
