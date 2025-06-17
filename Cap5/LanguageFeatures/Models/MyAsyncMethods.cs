331363e4f1b20f20ef1bfbe2e7c724af6c7a9a08
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-06-17 10:08:39 -0300

    Um método de ação assíncrona no arquivo HomeController.cs

---
+++ Cap5/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,34 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+
+        /// <summary>
+        /// Forma antiga
+        /// </summary>
+        /// <returns></returns>
+        public static Task<long?> GetPageLengthConfuse()
+        {
+            HttpClient client = new HttpClient();
+
+            var httpTask = client.GetAsync("http://manning.com");
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+            {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+
+
+        /// <summary>
+        /// Forma nova
+        /// </summary>
+        /// <returns></returns>
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpMessage = await client.GetAsync("http://manning.com");
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
