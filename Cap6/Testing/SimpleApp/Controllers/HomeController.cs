b1c19414d88cf89c360235feb7cc7dc5980f6061
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-06-22 19:04:26 -0300

    Adciona Moq aos teste unitários.

--- Cap6/Testing/SimpleApp/Controllers/HomeController.cs
+++ Cap6/Testing/SimpleApp/Controllers/HomeController.cs
@@ -5,9 +5,13 @@ namespace SimpleApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        public IDataSource dataSource = new ProductDataSource();
+
+
         public IActionResult Index()
         {
-            return View(Product.GetProducts());
+            return View(dataSource.Products);
         }
     }
 }
