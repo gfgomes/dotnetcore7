cebbfebd5a9cd01dbed7a29bc41a5655b9b3ba2a
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-07-09 18:25:09 -0300

    - Realiza busca de dados no banco localdb na página home/index - Realiza primeiro teste unitário da HomeController. - Conexão com localdb criada com sucesso.

---
+++ Cap7/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index() => View(repository.Products);
+    }
+}
--- Cap7/SportsSln/SportsStore/Program.cs
+++ Cap7/SportsSln/SportsStore/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//[builder.Services.]: configura todos os serviços necessários para a aplicação usando injeção de dependência
+
+//.AddControllersWithViews(): adiciona suporte para controladores e visualizações, permitindo que a aplicação use o padrão MVC (Model-View-Controller).
+
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Código padrão da criação do projeto que foi comantado
+//app.MapGet("/", () => "Hello World!");
+
+//permite suporte para servir conteúdo estático da pasta wwwroot
+app.UseStaticFiles();
+
+
+//Fornece o recurso de roteamento para endpoints, permitindo que a aplicação defina rotas para controladores(classes) e ações(metodos).
+app.MapDefaultControllerRoute();
+
+SeedData.EnsurePopulated(app);
 
 app.Run();
