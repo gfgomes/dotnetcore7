4a977a728014acf8b9dc4cad48da8216a6468705
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-07-23 16:49:46 -0300

    feat: simplificando a página do carrinho Razor

--- Cap7/SportsSln/SportsStore/Program.cs
+++ Cap7/SportsSln/SportsStore/Program.cs
@@ -22,6 +22,10 @@ builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+builder.Services.AddSingleton<IHttpContextAccessor,
+    HttpContextAccessor>();
+
 var app = builder.Build();
 
 //Código padrão da criação do projeto que foi comantado
