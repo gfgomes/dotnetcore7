cf023e8277858190615fae34ff6fa1be161eec7d
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-07-24 17:42:31 -0300

    feat(orders): cria e faz e o envio de pedidos; feat(products): adiciona crud de produtos;

--- Cap7/SportsSln/SportsStore/Models/Order.cs
+++ Cap7/SportsSln/SportsStore/Models/Order.cs
@@ -32,6 +32,9 @@ namespace SportsStore.Models
         public string? Country { get; set; }
 
         public bool GiftWrap { get; set; }
+
+        [BindNever]
+        public bool Shipped { get; set; }
     }
 
 }
