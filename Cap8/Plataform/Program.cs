65ccd4e3d9d702142da760c789575010a58566b0
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-07-28 16:48:28 -0300

    feat(app.Run())

--- Cap8/Plataform/Program.cs
+++ Cap8/Plataform/Program.cs
@@ -1,28 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Use(async (context, next) => {
-    await next();
-    await context.Response
-        .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+((IApplicationBuilder)app).Map("/branch", branch => {
+
+    branch.UseMiddleware<Platform.QueryStringMiddleWare>();
+
+    branch.Run(async (context) => {
+        await context.Response.WriteAsync($"Branch Middleware");
+    });
+
 });
 
-//Custom Middleware
-app.Use(async (context, next) => {
-    if (context.Request.Method == HttpMethods.Get
-            && context.Request.Query["custom"] == "true")
-    {
-        context.Response.ContentType = "text/plain";
-        await context.Response.WriteAsync("Custom Middleware \n");
-    }
-    await next();
-});
-
-//Class Middleware 
 app.UseMiddleware<Platform.QueryStringMiddleWare>();
 
-
-//Hello World!
 app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
