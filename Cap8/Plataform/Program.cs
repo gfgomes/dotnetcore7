7c4a25e159a8c66edb94b7c4ed314aa4b4985dfa
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-07-28 12:25:11 -0300

    feat: manipulação de middlewares.

--- Cap8/Plataform/Program.cs
+++ Cap8/Plataform/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) => {
+    await next();
+    await context.Response
+        .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+});
+
+//Custom Middleware
+app.Use(async (context, next) => {
+    if (context.Request.Method == HttpMethods.Get
+            && context.Request.Query["custom"] == "true")
+    {
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Custom Middleware \n");
+    }
+    await next();
+});
+
+//Class Middleware 
+app.UseMiddleware<Platform.QueryStringMiddleWare>();
+
+
+//Hello World!
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
