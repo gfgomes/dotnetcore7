79e70c442043d6ec06af8e8ac12c2f5018ffdeb7
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-07-30 15:03:21 -0300

    feat: usando options pattern em Program.cs

--- Cap8/Plataform/Program.cs
+++ Cap8/Plataform/Program.cs
@@ -1,11 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.Extensions.Options;
+using Plataform;
 
-((IApplicationBuilder)app).Map("/branch", branch => {
-    branch.Run(new Platform.QueryStringMiddleWare().Invoke);
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MessageOptions>(options => {
+    options.CityName = "Albany";
 });
 
-app.UseMiddleware<Platform.QueryStringMiddleWare>();
+var app = builder.Build();
+
+app.MapGet("/location", async (HttpContext context,
+    IOptions<MessageOptions> msgOpts) => {
+        Plataform.MessageOptions opts = msgOpts.Value;
+        await context.Response.WriteAsync($"{opts.CityName}, "
+            + opts.CountryName);
+    });
 
 app.MapGet("/", () => "Hello World!");
 
