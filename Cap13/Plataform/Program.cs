04437ee3a5af54fc14479caab84ab8ac0589ff9a
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-01 15:15:39 -0300

    Compreendendo o roteamento de URL

--- Cap13/Plataform/Program.cs
+++ Cap13/Plataform/Program.cs
@@ -1,15 +1,12 @@
 using Plataform;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.Configure<MessageOptions>(options => {
-    options.CityName = "Albany";
-});
-
 var app = builder.Build();
 
-app.UseMiddleware<LocationMiddleware>();
-
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<Population>();
+app.UseMiddleware<Capital>();
+app.Run(async (context) => {
+    await context.Response.WriteAsync("Terminal Middleware Reached");
+});
 
 app.Run();
