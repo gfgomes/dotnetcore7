6871783b1381dd8264593eb2aec0d8ccb28d213b
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-01 11:56:46 -0300

    cheap: rename cap 8 to 12 e create cap13

---
+++ Cap13/Plataform/Program.cs
@@ -0,0 +1,15 @@
+using Plataform;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MessageOptions>(options => {
+    options.CityName = "Albany";
+});
+
+var app = builder.Build();
+
+app.UseMiddleware<LocationMiddleware>();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
