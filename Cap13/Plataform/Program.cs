b38cf133e9fe1f048e0472d338e20d7d6b3df512
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-01 18:40:51 -0300

    Reapply "Simplificando o código no arquivo Program.cs na pasta"
    This reverts commit 04796cded96f2687dcbea7b59eef94101e9cca47.

--- Cap13/Plataform/Program.cs
+++ Cap13/Plataform/Program.cs
@@ -1,12 +1,31 @@
 using Plataform;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.UseMiddleware<Population>();
-app.UseMiddleware<Capital>();
-app.Run(async (context) => {
-    await context.Response.WriteAsync("Terminal Middleware Reached");
+app.UseRouting();
+
+//#pragma warning disable ASP0014
+
+//app.UseEndpoints(endpoints => {
+//    endpoints.MapGet("routing", async context => {
+//        await context.Response.WriteAsync("Request Was Routed"); 
+//    });
+
+
+//    endpoints.MapGet("capital/uk", new Capital().Invoke);
+//    endpoints.MapGet("population/paris", new Population().Invoke); 
+//});
+
+app.MapGet("routing", async context => {
+    await context.Response.WriteAsync("Request Was Routed");
 });
+app.MapGet("capital/uk", new Capital().Invoke);
+app.MapGet("population/paris", new Population().Invoke);
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Terminal Middleware Reached");
+//});
 
 app.Run();
