4d0d4bd06483c5a8a02bcfa6a3f97445cce42fc8
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-04 15:59:44 -0300

    feat: ajusta encondificação para UTF-8

--- Cap13/Plataform/Program.cs
+++ Cap13/Plataform/Program.cs
@@ -4,43 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-//app.MapGet("{first}/{second}/{third}", async context => {
-//    await context.Response.WriteAsync("Request Was Routed\n");
-//    foreach (var kvp in context.Request.RouteValues)
-//    {
-//        await context.Response
-//            .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-//    }
-//});
-
-//app.MapGet("{first}/{second}/{*catchall}", async context =>
-//{
-//    await context.Response.WriteAsync("Request Was Routed\n");
-//    foreach (var kvp in context.Request.RouteValues)
-//    {
-//        await context.Response
-//            .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-//    }
-//});
-
-//app.MapGet("{first:int}/{second:bool}", async context => {
-//    await context.Response.WriteAsync("Request Was Routed\n");
-//    foreach (var kvp in context.Request.RouteValues)
-//    {
-//        await context.Response
-//            .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-//    }
-//});
-
-//app.MapGet("files/{filename}.{ext}", async context => {
-//    await context.Response.WriteAsync("Request Was Routed\n");
-//    foreach (var kvp in context.Request.RouteValues)
-//    {
-//        await context.Response
-//            .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-//    }
-//});
-
 app.MapGet("{first:alpha:length(3)}/{second:bool}", async context => {
     await context.Response.WriteAsync("Request Was Routed\n");
     foreach (var kvp in context.Request.RouteValues)
@@ -50,14 +13,14 @@ app.MapGet("{first:alpha:length(3)}/{second:bool}", async context => {
     }
 });
 
-//app.MapGet("capital/{country}", Capital.Endpoint);
-//app.MapGet("capital/{country=France}", Capital.Endpoint);
-app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 
-//app.MapGet("population/{city}", Population.Endpoint);
-//app.MapGet("population/{city}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
-//app.MapGet("size/{city}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
-app.MapGet("size/{city?}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
+app.MapGet("capital/{country:regex(^uk|france|monaco$)}",
+    Capital.Endpoint);
+app.MapGet("size/{city?}", Population.Endpoint)
+    .WithMetadata(new RouteNameMetadata("population"));
 
+app.MapFallback(async context => {
+    await context.Response.WriteAsync("Routed to fallback endpoint");
+});
 
 app.Run();
