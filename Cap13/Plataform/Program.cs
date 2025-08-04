d437feaa1db71df666495a938f6292ec053a5fec
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-04 14:28:29 -0300

    feat: Correspondendo vários valores de um único segmento de URL

--- Cap13/Plataform/Program.cs
+++ Cap13/Plataform/Program.cs
@@ -13,6 +13,15 @@ app.MapGet("{first}/{second}/{third}", async context => {
     }
 });
 
+app.MapGet("files/{filename}.{ext}", async context => {
+    await context.Response.WriteAsync("Request Was Routed\n");
+    foreach (var kvp in context.Request.RouteValues)
+    {
+        await context.Response
+            .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+    }
+});
+
 app.MapGet("capital/{country}", Capital.Endpoint);
 //app.MapGet("population/{city}", Population.Endpoint);
 //app.MapGet("population/{city}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
