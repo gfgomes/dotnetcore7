c5057b62aa6d4ee4ee5d1a2bb93b97360de38cd3
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-10 23:58:07 -0300

    feat: utiliza injeção de dependencia do asp.net core após testar a opções com singleton e com type broker

--- Cap14/Plataform/Program.cs
+++ Cap14/Plataform/Program.cs
@@ -4,16 +4,27 @@ using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
+
+
 var app = builder.Build();
 
 app.UseMiddleware<WeatherMiddleware>();
 
 app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
 
-IResponseFormatter formatter = new TextResponseFormatter();
-app.MapGet("endpoint/function", async context => {
-    await formatter.Format(context,
-        "Endpoint Function: It is sunny in LA");
-});
+//IResponseFormatter formatter = TextResponseFormatter.Singleton;
+//IResponseFormatter formatter = TypeBroker.Formatter;
+
+//app.MapGet("endpoint/function", async context => {
+//    await formatter.Format(context,
+//        "Endpoint Function: It is sunny in LA");
+//});
+
+app.MapGet("endpoint/function",
+    async (HttpContext context, IResponseFormatter formatter) => {
+        await formatter.Format(context,
+            "Endpoint Function: It is sunny in LA");
+    });
 
 app.Run();
--- Cap14/Plataform/Services/TextResponseFormatter.cs
+++ Cap14/Plataform/Services/TextResponseFormatter.cs
@@ -3,11 +3,25 @@
     public class TextResponseFormatter : IResponseFormatter
     {
         private int responseCounter = 0;
+        private static TextResponseFormatter? shared;
 
         public async Task Format(HttpContext context, string content)
         {
             await context.Response.
                 WriteAsync($"Response {++responseCounter}:\n{content}");
+        }
+
+        //Singleton instance
+        public static TextResponseFormatter Singleton
+        {
+            get
+            {
+                if (shared == null)
+                {
+                    shared = new TextResponseFormatter();
+                }
+                return shared;
+            }
         }
     }
 }
