bca37453d69d73c99ca035e48c42b543f9263fbe
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-21 19:23:04 -0300

    feat: Usando tipos não vinculados em serviços

--- Cap14/Plataform/Program.cs
+++ Cap14/Plataform/Program.cs
@@ -1,44 +1,47 @@
-//using Platform;
-using Platform.Services;
+//using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//IConfiguration config = builder.Configuration;
+//builder.Services.AddScoped<IResponseFormatter, TextResponseFormatter>(); 
+//builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
 
-//builder.Services.AddScoped<IResponseFormatter>(serviceProvider => { 
-//    string? typeName = config["services:IResponseFormatter"];
-//    return (IResponseFormatter)ActivatorUtilities
-//        .CreateInstance(serviceProvider, typeName == null
-//            ? typeof(GuidService) : Type.GetType(typeName, true)!); 
-//});
-//builder.Services.AddScoped<ITimeStamper, DefaultTimeStamper>();
 
-builder.Services.AddScoped<IResponseFormatter, TextResponseFormatter>();
-builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
-builder.Services.AddScoped<IResponseFormatter, GuidService>();
+//builder.Services.AddScoped<IResponseFormatter, GuidService>();
+
+builder.Services.AddSingleton(typeof(ICollection<>), typeof(List<>));
 
 var app = builder.Build();
 
-//app.UseMiddleware<WeatherMiddleware>();
-
-//app.MapEndpoint<WeatherEndpoint>("endpoint/class");
-
-//app.MapGet("endpoint/function", 
-//    async (HttpContext context, IResponseFormatter formatter) => { 
-//        await formatter.Format(context, 
-//            "Endpoint Function: It is sunny in LA");
+//app.MapGet("single", async context => {
+//    IResponseFormatter formatter = context.RequestServices 
+//        .GetRequiredService<IResponseFormatter>();
+//    await formatter.Format(context, "Single service"); 
 //});
 
-app.MapGet("single", async context => {
-    IResponseFormatter formatter = context.RequestServices
-        .GetRequiredService<IResponseFormatter>();
-    await formatter.Format(context, "Single service");
+//app.MapGet("/", async context => {
+//    IResponseFormatter formatter = context.RequestServices
+//        .GetServices<IResponseFormatter>().First(f => f.RichOutput); 
+//    await formatter.Format(context, "Multiple services");
+//});
+
+app.MapGet("string", async context => {
+    ICollection<string> collection = context.RequestServices
+        .GetRequiredService<ICollection<string>>();
+    collection.Add($"Request: {DateTime.Now.ToLongTimeString()}");
+    foreach (string str in collection)
+    {
+        await context.Response.WriteAsync($"String: {str}\n");
+    }
 });
 
-app.MapGet("/", async context => {
-    IResponseFormatter formatter = context.RequestServices
-        .GetServices<IResponseFormatter>().First(f => f.RichOutput);
-    await formatter.Format(context, "Multiple services");
+app.MapGet("int", async context => {
+    ICollection<int> collection
+        = context.RequestServices.GetRequiredService<ICollection<int>>();
+    collection.Add(collection.Count() + 1);
+    foreach (int val in collection)
+    {
+        await context.Response.WriteAsync($"Int: {val}\n");
+    }
 });
 
 app.Run();
