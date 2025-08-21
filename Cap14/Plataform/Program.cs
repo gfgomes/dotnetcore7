1dc4ff951c7a797ecd185b6e1c41f215a838d2f4
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-08-21 12:04:17 -0300

    feat: ciclo de vida de serviços

--- Cap14/Plataform/Program.cs
+++ Cap14/Plataform/Program.cs
@@ -5,7 +5,8 @@ using Platform.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
-builder.Services.AddTransient<IResponseFormatter, GuidService>();
+//builder.Services.AddTransient<IResponseFormatter, GuidService>();
+builder.Services.AddScoped<IResponseFormatter, GuidService>();
 
 var app = builder.Build();
 
--- Cap14/Plataform/Services/EndpointExtensions.cs
+++ Cap14/Plataform/Services/EndpointExtensions.cs
@@ -2,26 +2,35 @@
 
 namespace Microsoft.AspNetCore.Builder
 {
+
     public static class EndpointExtensions
     {
-        public static void MapEndpoint<T>(this IEndpointRouteBuilder app, string path, string methodName = "Endpoint")
+
+        public static void MapEndpoint<T>(this IEndpointRouteBuilder app,
+                string path, string methodName = "Endpoint")
         {
+
             MethodInfo? methodInfo = typeof(T).GetMethod(methodName);
             if (methodInfo?.ReturnType != typeof(Task))
             {
                 throw new System.Exception("Method cannot be used");
             }
-
-            T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
+            //T endpointInstance =
+            //ActivatorUtilities.CreateInstance<T> (app.ServiceProvider); 
 
             ParameterInfo[] methodParams = methodInfo!.GetParameters();
 
-            app.MapGet(path, context =>
-                (Task)methodInfo.Invoke(endpointInstance,
-                  methodParams.Select(p =>
-                    p.ParameterType == typeof(HttpContext) ? context
-                      : app.ServiceProvider.GetService(p.ParameterType))
-                    .ToArray())!);
+            app.MapGet(path, context => {
+                T endpointInstance =
+                    ActivatorUtilities.CreateInstance<T>
+                        (context.RequestServices);
+                return (Task)methodInfo.Invoke(endpointInstance!,
+                    methodParams.Select(p =>
+                    p.ParameterType == typeof(HttpContext)
+                    ? context
+                    : context.RequestServices.GetService(p.ParameterType))
+                        .ToArray())!;
+            });
         }
     }
 }
