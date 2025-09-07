ee9be70de9166331ea64080af2c22add9dcd0a62
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-09-07 18:42:12 -0300

    feat: armazenando dados em cache na memoria

--- Cap17/Plataform/Program.cs
+++ Cap17/Plataform/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDistributedMemoryCache(opts => {
+    opts.SizeLimit = 200;
+});
+
 var app = builder.Build();
+
+app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
 
 app.MapGet("/", async context => {
     await context.Response.WriteAsync("Hello World!");
