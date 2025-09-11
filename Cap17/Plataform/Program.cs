1627b0273f192a8c6d0fe4ac2b420d8955a7a7ba
Gabriel Gomes <gabriel.fgomes@gmail.com>
2025-09-11 00:59:30 -0300

    .

--- Cap17/Plataform/Program.cs
+++ Cap17/Plataform/Program.cs
@@ -1,15 +1,23 @@
+using Platform.Services;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDistributedMemoryCache(opts => {
-    opts.SizeLimit = 200;
+builder.Services.AddDistributedSqlServerCache(opts =>
+{
+    opts.ConnectionString = builder.Configuration["ConnectionStrings:CacheConnection"];
+    opts.SchemaName = "dbo";
+    opts.TableName = "DataCache";
 });
 
-var app = builder.Build();
+builder.Services.AddResponseCaching();
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
 
+var app = builder.Build();
+app.UseResponseCaching();
 app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
 
-app.MapGet("/", async context => {
+app.MapGet("/", async context =>
+{
     await context.Response.WriteAsync("Hello World!");
 });
 
-app.Run();
+app.Run();  
