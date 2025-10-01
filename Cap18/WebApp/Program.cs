c22e5bc02e23a43958386de567635c74749db1f8
gabriel.fgomes@gmail.com <gabriel.fgomes@gmail.com>
2025-10-01 18:38:54 -0300

    docs: explica motivo da criação do escopo.

--- Cap18/WebApp/Program.cs
+++ Cap18/WebApp/Program.cs
@@ -4,8 +4,7 @@ using WebApp.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(opts => {
-    opts.UseSqlServer(builder.Configuration[
-        "ConnectionStrings:ProductConnection"]);
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
     opts.EnableSensitiveDataLogging(true);
 });
 
@@ -13,6 +12,11 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+
+//Cria uma scope e obtem o serviço necessário para popular o banco de dados
+// (essa abordagem é necessária porque o DataContext é registrado como scoped por padrão)
+// como scoped, ele é criado uma vez por requisição, e não uma vez por aplicação
+// essa abordagem faz o papel que um request faria, mas sem a necessidade de um request
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
 
 SeedData.SeedDatabase(context);
