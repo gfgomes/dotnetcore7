a25241e45f8dc0cf3ed6d636ad4711296f2ca668
gabriel.fgomes@gmail.com <gabriel.fgomes@gmail.com>
2025-10-10 18:23:40 -0300

    feat: usa views

--- Cap21/WebApp/Program.cs
+++ Cap21/WebApp/Program.cs
@@ -1,73 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.RateLimiting;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<DataContext>(opts =>
-{
+builder.Services.AddDbContext<DataContext>(opts => {
     opts.UseSqlServer(builder.Configuration[
         "ConnectionStrings:ProductConnection"]);
     opts.EnableSensitiveDataLogging(true);
 });
 
-builder.Services.AddControllers()
-    .AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
-
-builder.Services.AddRateLimiter(opts =>
-{
-    opts.AddFixedWindowLimiter("fixedWindow", fixOpts =>
-    {
-        fixOpts.PermitLimit = 1;
-        fixOpts.QueueLimit = 0;
-        fixOpts.Window = TimeSpan.FromSeconds(15);
-    });
-});
-
-builder.Services.Configure<MvcNewtonsoftJsonOptions>(opts =>
-{
-    opts.SerializerSettings.NullValueHandling
-        = Newtonsoft.Json.NullValueHandling.Ignore;
-    opts.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-});
-
-builder.Services.Configure<MvcOptions>(options =>
-{
-    options.RespectBrowserAcceptHeader = true;   // respeita o Accept do cliente
-    options.ReturnHttpNotAcceptable = true;      // envia 406 se não suportar o tipo
-});
-
-builder.Services.AddOutputCache(opts => {
-    opts.AddPolicy("30sec", policy => {
-        policy.Cache();
-        policy.Expire(TimeSpan.FromSeconds(30));
-    });
-});
-
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "WebApp",
-        Version = "v1"
-    });
-});
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseRateLimiter();
-
-app.UseOutputCache();
-
+app.UseStaticFiles();
 app.MapControllers();
-
-app.MapGet("/", () => "Hello World!");
-
-app.UseSwagger();
-app.UseSwaggerUI(options => {
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApp");
-});
+app.MapControllerRoute("Default",
+    "{controller=Home}/{action=Index}/{id?}"); // mesmo padrão que definir app.MapDefaultControllerRoute();
 
 var context = app.Services.CreateScope().ServiceProvider
     .GetRequiredService<DataContext>();
