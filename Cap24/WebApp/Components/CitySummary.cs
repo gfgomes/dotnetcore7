31cfda427f35429d538f667b33d64089ed72f7d5
gabriel.fgomes@gmail.com <gabriel.fgomes@gmail.com>
2025-10-17 11:26:49 -0300

    feat: utiliza os tres tipos de retorno do IViewComponent usando um view component (vc)

--- Cap24/WebApp/Components/CitySummary.cs
+++ Cap24/WebApp/Components/CitySummary.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using WebApp.Models;
 
 namespace WebApp.Components
@@ -12,13 +14,30 @@ namespace WebApp.Components
             data = cdata;
         }
 
+        // Exemplo retornando uma View com um ViewModel (Views\Shared\Components\CitySummary\Default.cshtml)
+        //public IViewComponentResult Invoke()
+        //{
+        //    return View(new CityViewModel
+        //    {
+        //        Cities = data.Cities.Count(),
+        //        Population = data.Cities.Sum(c => c.Population)
+        //    });
+        //}
+
+        //Exemplo retornando fragmento de html que será codificada para ficar segura e não ser rederizada como html(This is a &lt;h3&gt;&lt;i&gt;string&lt;/i&gt;&lt;/h3&gt;)
+        //Além do método Content, o método Invoke pode retornar um string, que será 
+        //automaticamente convertido em ContentViewComponentResult.
+        //public IViewComponentResult Invoke()
+        //{
+        //    return Content("This is a <h3><i>string</i></h3>");
+        //}
+
+        //Exemplo retornando fragmento de html que não será codificada, ou seja, será renderizada como html (This is a <h3><i>string</i></h3>)
+        //Deve ter cuidado ao usar HtmlString para evitar vulnerabilidades XSS.
         public IViewComponentResult Invoke()
         {
-            return View(new CityViewModel
-            {
-                Cities = data.Cities.Count(),
-                Population = data.Cities.Sum(c => c.Population)
-            });
+            return new HtmlContentViewComponentResult(
+                new HtmlString("This is a <h3><i>string</i></h3>"));
         }
     }
 }
